a9c510162bd76c8798500cf28a5acca6aaf70223
Anna Lebiedziewicz <nalin.kane@gmail.com>
2019-12-14 19:43:16 +0000

    added read and write file

--- index.js
+++ index.js
@@ -13,10 +13,31 @@ app.use(express.static(publicFolder));
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, `${publicFolder}/notes.html`));
 });
+
 app.get("/api/notes", function(req, res) {
   fs.readFile("./db/db.json", (err, data) => {
     if (err) throw err;
     return res.json(JSON.parse(data));
+  });
+});
+
+app.post("/api/notes", function(req, res) {
+  const newNote = req.body;
+  console.log(typeof req.body);
+  //step 2 parse as JSON
+  //push new data to the file
+  //write to file and console the message
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const notesFile = JSON.parse(data);
+
+    notesFile.push(newNote);
+    const json = JSON.stringify(notesFile);
+    console.log(notesFile);
+    fs.writeFile("./db/db.json", json, "utf8", (err, data) => {
+      if (err) throw err;
+      res.send("Note has been written");
+    });
   });
 });
 
